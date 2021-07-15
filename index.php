179a304209d9bb721c82f4fe6cc2526a42f4f197
Thomas ROGER <thomaroger@gmail.com>
2021-07-15 09:34:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Thomas ROGER">
-    <title>Home - Saint Pierre du Perray</title>
+    <title>Home - Bogève</title>
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="./web/style.css" rel="stylesheet">
